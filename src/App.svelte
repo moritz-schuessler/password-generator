b830bf10e41ec8480ed003e34c68ddde10530fc8
Mosch <mail@moritzschuessler.de>
2023-06-04 03:56:35 +0200

    Added functionality to generate a password

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,12 @@
 <script>
+import PasswordWrapper from "./lib/password/PasswordWrapper.svelte";
+import Password from "./lib/password/Password.svelte";
 </script>
+
+
+<PasswordWrapper>
+    <Password />
+</PasswordWrapper>
 
 <style>
 
---
+++ src/lib/password/Password.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { length, characters } from "../stores.js";
+
+    $: password = generatePassword()
+
+    const generatePassword = () => {
+        let temp = '';
+        for (let i = 0; i < $length; i++) {
+            const charAt = Math.floor(Math.random() * $characters.length);
+            temp += $characters.charAt(charAt);
+        }
+        return temp
+    }
+</script>
+
+{password}
+
+
+

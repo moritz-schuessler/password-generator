024281c5e1205b0d44b0a92feb3744a8bd03d9f2
Mosch <mail@moritzschuessler.de>
2023-06-06 22:03:37 +0200

    Changed Styling of the Setting-Components

--- src/lib/settings/SettingsWrapper.svelte
+++ src/lib/settings/SettingsWrapper.svelte
@@ -7,7 +7,8 @@
         display: flex;
         flex-direction: column;
         align-items: center;
-        justify-content: center;
+        justify-content: space-between;
+        gap: 1rem;
 
         height: 100vh;
         width: 60vw;
